f01ccabb45ea68552b9cf980541b22abf23c63fa
joepearson95 <joe.c.pearson95@gmail.com>
2021-02-16 17:26:11 +0000

    update

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/dashboard.html', {'products':products})
 
 def products(request):
     return render(request, 'accounts/products.html')
